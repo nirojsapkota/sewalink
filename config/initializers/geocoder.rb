e12ceba940af489238305bc0217698b0a2d421fd
Niroj Sapkota <nirojsapkota15@gmail.com>
2026-04-17 15:50:02 +1000

    feat(geocoding): Configure Geocoder to use Google Maps API

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,10 @@
 Geocoder.configure(
   # Geocoding options
-  # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
-  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
-  # use_https: false,           # use HTTPS for lookup requests? (if supported)
-  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
+  timeout: 5,                 # geocoding service timeout (secs)
+  lookup: :google,            # name of geocoding service (symbol)
+  api_key: ENV['GOOGLE_GEOCODING_API_KEY'], # API key for geocoding service
+  use_https: true,            # use HTTPS for lookup requests
+  language: :en,              # ISO-639 language code
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
@@ -16,8 +12,8 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
-  # distances: :linear          # :spherical or :linear
+  units: :km,                 # :km for kilometers or :mi for miles
+  distances: :spherical       # :spherical or :linear
 
   # Cache configuration
   # cache_options: {

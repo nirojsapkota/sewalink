5286549c28b9f26e5f7a49f8bd6d0ceaae98ceb5
Niroj Sapkota <nirojsapkota15@gmail.com>
2026-09-04 14:52:20 +1000

    test(10-01): add failing spec for role-based admin access control

---
+++ spec/requests/admin/access_control_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::AccessControl", type: :request do
+  describe "GET /admin (admin_root_path)" do
+    it "allows a super_admin user" do
+      user = create(:user, :super_admin)
+      sign_in user
+
+      get admin_root_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "allows an accountant user" do
+      user = create(:user, :accountant)
+      sign_in user
+
+      get admin_root_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "denies a plain user with no role and admin: false" do
+      user = create(:user)
+      sign_in user
+
+      get admin_root_path
+
+      expect(response).to redirect_to(root_path)
+      follow_redirect!
+      expect(response.body).to include("Access denied. Admin only.")
+    end
+
+    it "denies a user with admin: true but no rolify role (boolean fully decoupled)" do
+      user = create(:user, admin: true)
+      sign_in user
+
+      get admin_root_path
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "allows a user created via the legacy :admin trait (backward compatible)" do
+      user = create(:user, :admin)
+      sign_in user
+
+      get admin_root_path
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end

383bdb65daaaae67d460b3559be332dfc8ac1510
Niroj Sapkota <nirojsapkota15@gmail.com>
2026-09-04 13:19:26 +1000

    feat(09-02): add edit/update/suspend/reactivate actions to Admin::UsersController
    - self-suspend guard prevents admin lockout
    - strong params exclude phone/email/encrypted_password/otp_secret
    - all mutating actions log_admin_action! for audit trail

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: [:show, :edit, :update, :suspend, :reactivate]
+
   def index
     @users = User.order(created_at: :desc)
     if params[:query].present?
@@ -8,9 +10,46 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def show
-    @user = User.find(params[:id])
     @tasks_posted = @user.tasks.count
     @tasks_completed_as_tasker = Task.joins(:accepted_bid).where(bids: { user_id: @user.id }).where(status: :completed).count
     @tasks_completed_as_poster = @user.tasks.completed.count
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      log_admin_action!("update_user", @user, details: { changes: user_params.to_h })
+      redirect_to admin_user_path(@user), notice: "User updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def suspend
+    if @user == current_user
+      redirect_to admin_user_path(@user), alert: "You cannot suspend your own account." and return
+    end
+
+    @user.suspend!(params[:suspension_reason].presence || "No reason provided")
+    log_admin_action!("suspend_user", @user, details: { reason: @user.suspension_reason })
+    redirect_to admin_user_path(@user), notice: "User suspended."
+  end
+
+  def reactivate
+    @user.reactivate!
+    log_admin_action!("reactivate_user", @user)
+    redirect_to admin_user_path(@user), notice: "User reactivated."
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :bio, :locale, :admin, :active_role)
   end
 end
